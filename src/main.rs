213f91c5fefd124e6eefc663d454cfbbd08e9ce8
Federico Londoño <felova21@gmail.com>
2024-05-06 21:53:13 +0200

    initial

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    MainWindow::new().unwrap().run().unwrap();
+}
+
+slint::slint! {
+component MemoryTile inherits Rectangle {
+    width: 64px;
+    height: 64px;
+    background: #3960D5;
+
+    Image {
+        source: @image-url("./icons/bus.png");
+        width: parent.width;
+        height: parent.height;
+    }
+}
+
+export component MainWindow inherits Window {
+    MemoryTile {}
+}
+
+}
